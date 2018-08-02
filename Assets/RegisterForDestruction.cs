29b89db57294db970d16669bc9f1a6907097bddb
Matt Schell <mirrorfishmedia@gmail.com>
2018-08-02 18:59:32 -0400

    added registering for destruction

---
+++ Assets/RegisterForDestruction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterForDestruction : MonoBehaviour {
+
+    private void OnEnable()
+    {
+        GameMan.gm.ScheduleForDestruction(this.gameObject);
+    }
+}
