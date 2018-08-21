9bc6d5ff4c7175b683dbdaa665025b39f6342747
Matt Schell <mirrorfishmedia@gmail.com>
2018-08-21 10:18:45 +0200

    reimplement cubeworld

---
+++ Assets/Strata/Scripts/BSPGeneration/BSPLeaf.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Strata
+{
+    public class BSPLeaf
+    {
+        public GridPosition origin;
+        public int width;
+        public int height;
+        public BSPLeaf childA;
+        public BSPLeaf childB;
+        public int splitHorizontalChance = 50;
+        public int minLeafSize = 6;
+
+        public BSPLeaf(GridPosition inputOrigin, int inputWidth, int inputHeight)
+        {
+            origin = inputOrigin;
+            width = inputWidth;
+            height = inputHeight;
+        }
+
+        public bool Split()
+        {
+            if (childA != null || childB != null)
+            {
+                //Already split, go no further;
+                return false;
+            }
+
+            bool splitHorizontal = RollPercentage(splitHorizontalChance);
+
+            if (width > height && width / height >= 1.25)
+                splitHorizontal = false;
+            else if (height > width && height / width >= 1.25)
+                splitHorizontal = true;
+
+            int maxLeafSize = (splitHorizontal ? height : width) - minLeafSize;
+            if (maxLeafSize <= minLeafSize)
+            {
+                return false;
+            }
+
+            int splitPosition = Random.Range(minLeafSize, maxLeafSize);
+
+            if (splitHorizontal)
+            {
+                childA = new BSPLeaf(origin, width, splitPosition);
+                GridPosition childBOrigin = new GridPosition(origin.x, origin.y + splitPosition);
+                childB = new BSPLeaf(childBOrigin, width, height - splitPosition);
+            }
+            else
+            {
+                childA = new BSPLeaf(origin, splitPosition, height);
+                GridPosition childBOrigin = new GridPosition(origin.x + splitPosition, origin.y);
+                childA = new BSPLeaf(childBOrigin, width - splitPosition, height);
+
+            }
+            //Division successful
+            return true;
+
+
+        }
+
+        public bool RollPercentage(int chanceToHit)
+        {
+            int randomResult = Random.Range(0, 100);
+            if (randomResult < chanceToHit)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+
+}
---
+++ Assets/Strata/Scripts/BSPGeneration/GeneratorBinarySpacePartitioner.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Strata
+{
+    [CreateAssetMenu(menuName = "Strata/Generators/Generator Binary Space Partitioner")]
+    public class GeneratorBinarySpacePartitioner : Generator
+    {
+        public int maxLeafSize = 20;
+
+        private BSPLeaf leaf;
+
+        public override bool Generate(BoardGenerator boardGenerator)
+        {
+            SplitLeaves(boardGenerator);
+            return true;
+        }
+
+        public void SplitLeaves(BoardGenerator boardGenerator)
+        {
+            //Create the first root leaf that fills the whole board, we'll split this
+            BSPLeaf rootLeaf = new BSPLeaf(new GridPosition(0, 0), boardGenerator.profile.boardHorizontalSize, boardGenerator.profile.boardVerticalSize);
+
+            List<BSPLeaf> leafList = new List<BSPLeaf>();
+
+            leafList.Add(rootLeaf);
+
+            bool splitLeaf = true;
+
+            Debug.Log("leafList count " + leafList.Count);
+
+            while (splitLeaf)
+            {
+                splitLeaf = false;
+
+                for (int i = 0; i < leafList.Count; i++)
+                {
+                    Debug.Log("leafList i " + i + "leaf origin " + leafList[i].origin.x + " " + leafList[i].origin.y);
+                    if (leafList[i].childA == null && leafList[i].childB == null)
+                    {
+                        if (leafList[i].width > maxLeafSize || leafList[i].height > maxLeafSize || RollPercentage(75))
+                        {
+                            if (leafList[i].Split())
+                            {
+                                leafList.Add(leafList[i].childA);
+                                leafList.Add(leafList[i].childB);
+                                splitLeaf = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            DrawLeaves(boardGenerator, leafList);
+        }
+
+        public void DrawLeaves(BoardGenerator boardGenerator, List<BSPLeaf> leafList)
+        {
+            for (int i = 0; i < leafList.Count; i++)
+            {
+                Debug.Log("leafList " + leafList[i].origin + " height " + leafList[i].height + " width " + leafList[i].height);
+            }
+        }
+
+        public bool RollPercentage(int chanceToHit)
+        {
+            int randomResult = Random.Range(0, 100);
+            if (randomResult < chanceToHit)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+}
+
